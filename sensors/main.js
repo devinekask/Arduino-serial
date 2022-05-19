8aba89ea8e482b304eaa59b0cfd582e0810bbc91
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2022-05-19 20:38:54 +0200

    SerialBinary & SerialText

--- sensors/main.js
+++ sensors/main.js
@@ -1,6 +1,69 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import { SerialBinary } from "../lib/index.js";
+import { sketch } from 'p5js-wrapper';
+
+let serial;
+let vector;
+let bg = 200;
+
+const init = async () => {
+  serial = new SerialBinary({ log: false });
+
+  serial.onMessage(handleIncomingMessage);
+
+  document.querySelector("#connect").addEventListener('click', async (e) => {
+    if (!serial.port) {
+      await serial.connect();
+      serial.write(new Uint8Array([65]));
+      e.target.innerText = "Disconnect";
+    } else {
+      await serial.disconnect();
+      e.target.innerText = "Connect";
+    }
+  })
+
+  sketch.setup = function () {
+    createCanvas(800, 600);
+    vector = createVector(0, 0);
+  };
+
+  sketch.draw = function () {
+    background(bg);
+    strokeWeight(4);
+    push();
+    translate(width / 2, height / 2);
+    rotate(vector.heading());
+    line(0, 0, vector.mag() * 250, 0);
+    pop();
+  };
+
+}
+
+
+const handleIncomingMessage = message => {
+  if (message[0] == 65) {
+    //serial.clear();
+    serial.write(new Uint8Array([65]));
+  } else {
+    const [potmeter1, potmeter2, button] = message;
+    const x = map(potmeter1, 0, 255, -1, 1);
+    const y = map(potmeter2, 0, 255, -1, 1);
+    vector.set(x, y)
+    if (button > 0) {
+      bg = "red";
+    } else {
+      bg = 200;
+    }
+
+    serial.write(new Uint8Array([65]));
+  }
+}
+
+
+
+
+
+
+init();
+
--- web-tutorial/main.js
+++ web-tutorial/main.js
@@ -1,88 +1,8 @@
+import { SerialText } from '../lib';
 import './style.css'
 
-let port;
-let reader;
-let inputDone;
-let outputDone;
-let inputStream;
-let outputStream;
+let serial;
 
-const filters = [
-  { usbVendorId: 9025, usbProductId: 32823 } //arduino micro, see chrome://device-log/
-];
-
-const connect = async (defaultPort) => {
-  port = defaultPort;
-  if (!port) {
-    port = await navigator.serial.requestPort({ filters });
-  }
-
-  await port.open({ baudRate: 9600 });
-
-  let decoder = new window.TextDecoderStream();
-  inputDone = port.readable.pipeTo(decoder.writable);
-  inputStream = decoder.readable;
-
-  reader = inputStream.getReader();
-  readLoop();
-
-  const encoder = new window.TextEncoderStream();
-  outputDone = encoder.readable.pipeTo(port.writable);
-  outputStream = encoder.writable;
-
-  writeToStream('Lets go!');
-  toggleUIConnected(true);
-}
-
-const disconnect = async () => {
-  if (reader) {
-    await reader.cancel();
-    await inputDone.catch(() => { });
-    reader = null;
-    inputDone = null;
-  }
-
-  if (outputStream) {
-    await outputStream.getWriter().close();
-    await outputDone;
-    outputStream = null;
-    outputDone = null;
-  }
-
-  await port.close();
-  port = null;
-}
-
-const readLoop = async () => {
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const { value, done } = await reader.read();
-    if (value) {
-      console.log('[RECEIVED]', value);
-      addToReceived(value);
-    }
-    if (done) {
-      console.log('[readLoop] DONE', done);
-      reader.releaseLock();
-      break;
-    }
-  }
-}
-
-const writeToStream = line => {
-  if (!outputStream || line.trim() === "") return;
-  const writer = outputStream.getWriter();
-  console.log('[SEND]', line);
-  writer.write(line);
-  writer.releaseLock();
-}
-
-const tryToConnect = async () => {
-  const ports = await navigator.serial.getPorts({ filters });
-  if (ports.length > 0) {
-    await connect(ports[0]);
-  }
-}
 
 const addToReceived = line => {
   document.querySelector('.received').textContent = line + document.querySelector('.received').textContent;
@@ -105,23 +25,26 @@ const setupEventListeners = () => {
 }
 
 const handleClickConnect = async () => {
-  if (port) {
-    await disconnect();
+  if (!serial.port) {
+    await serial.connect();
+    toggleUIConnected(true);
+    serial.write('Lets go!');
+  } else {
+    await serial.disconnect()
     toggleUIConnected(false);
-    return;
   }
-  await connect();
 }
 
 const handleFormSubmit = e => {
   e.preventDefault();
-  writeToStream(e.target.message.value);
+  serial.write(e.target.message.value);
   e.target.reset();
 }
 
 const init = () => {
+  serial = new SerialText({ log: false });
+  serial.onMessage(addToReceived);
   setupEventListeners();
-  tryToConnect();
 }
 
 init();
